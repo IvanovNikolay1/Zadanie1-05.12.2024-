7cbdcee385280f7dc98d3c8f3b47fff5410ea875
IvanovNR <IvanovNR@310D-12>
2024-12-05 09:35:11 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        public static int CountBits(int num)
+        {
+            string binarRepresentation = Convert.ToString(num, 2);
+
+            int count = 0;
+
+            for (int i = 0; i < binarRepresentation.Length; i++)
+            {
+                if (binarRepresentation[i] == '1')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число для подсчета значащих битов: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int num))
+            {
+                int count = CountBits(num);
+                Console.WriteLine($"Количество значащих битов в числе {num} равно: {count}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка. Введено не коректное число.");
+            }
+        }
+    }
+}
